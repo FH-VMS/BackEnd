d2e126e40f0118edf71f97fde934dcca356b40a6
hunterlegend <15482@etransfar.com>
2017-07-28 12:19:05 +0800

    add

---
+++ Model/Withdraw/MachineMoneyModel.cs
@@ -0,0 +1,33 @@
+﻿using Model.Sys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Withdraw
+{
+    [Table("table_machine_money")]
+    public class MachineMoneyModel
+    {
+        [Column(Name = "machine_id")]
+        public string MachineId
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ali_money")]
+        public float AliMoney
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "wx_money")]
+        public float WxMoney
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Model/Withdraw/TotalMoneyModel.cs
@@ -0,0 +1,47 @@
+﻿using Model.Sys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Withdraw
+{
+    [Table("table_total_money")]
+    public class TotalMoneyModel
+    {
+        [Column(Name = "id")]
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "client_id")]
+        public string ClientId
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ali_account")]
+        public string AliAccount
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "wx_account")]
+        public string WxAccount
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "total_money")]
+        public float TotalMoney
+        {
+            get;
+            set;
+        }
+    }
+}
