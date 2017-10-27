0e00a0c4e84fab1dd97b0a1bde808c0a39f5b5ec
KJTPAY\lilangui <850899364@qq.com>
2017-10-27 18:37:12 +0800

    自动获取ip

--- NETIOCPSvr/Program.cs
+++ NETIOCPSvr/Program.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using log4net;
 using System.Configuration;
 using System.IO;
+using System.Net.Sockets;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -45,7 +46,20 @@ namespace SocketAsyncSvr
             AsyncSocketSvr = new AsyncSocketServer(parallelNum);
             AsyncSocketSvr.SocketTimeOutMS = socketTimeOutMS;
             AsyncSocketSvr.Init();
-            IPEndPoint listenPoint = new IPEndPoint(IPAddress.Parse("192.168.20.190"), port);
+            string HostName = Dns.GetHostName(); //得到主机名
+            IPHostEntry IpEntry = Dns.GetHostEntry(HostName);
+            string ipV4 = string.Empty;
+            for (int i = 0; i < IpEntry.AddressList.Length; i++)
+            {
+                //从IP地址列表中筛选出IPv4类型的IP地址
+                //AddressFamily.InterNetwork表示此IP为IPv4,
+                //AddressFamily.InterNetworkV6表示此地址为IPv6类型
+                if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipV4 = IpEntry.AddressList[i].ToString();
+                }
+            }
+            IPEndPoint listenPoint = new IPEndPoint(IPAddress.Parse(ipV4), port);
             AsyncSocketSvr.Start(listenPoint);
 
             Console.WriteLine("Press any key to terminate the server process....");
