c78f0c5e8292325efa20b0599949fd25590e68ae
landry-PC\landry <850899364@qq.com>
2018-03-14 21:27:54 +0800

    检测微信升级后证书

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using PaymentLib.wx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chuang.Back.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string GetTestWpay()
+        {
+            WxPayData jsApiParam = new WxPayData();
+            jsApiParam.SetValue("mch_id", "mch_id");
+            jsApiParam.SetValue("nonce_str", WxPayApi.GenerateNonceStr());
+            jsApiParam.SetValue("sign", jsApiParam.MakeSign());
+
+            return HttpService.Post(jsApiParam.ToXml(), "https://apitest.mch.weixin.qq.com/sandboxnew/pay/getsignkey", true, 5000);
+        
+        }
+    }
+}
